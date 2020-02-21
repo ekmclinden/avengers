f855a590ca5a80c0a723fd9ab745b41b417611d2
Erin McLinden <ekmclinden@gmail.com>
2020-02-20 20:37:20 -0800

    add hide show elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,5 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
+var add = function(number1, number2, number3) {
+  return number1 + number2 + number3;
 };
 
 $(document).ready(function() {
@@ -19,15 +7,21 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-   
-if (result <= 6) {
+    var number3 = parseInt($("#add2").val());
+    var result = add(number1, number2, number3);
+    $("#output").text(result); 
+
+if (number1===0) {
+  $(".warning").show();
+}
+if (result < 4) {
   $("#mcdonalds").hide();
   $("#home").show();
 }
-  if (result > 6) {
+  if (result > 7) {
     $("#home").hide();
     $("#mcdonalds").show();
-}
-});
-});
+    }
+
+  }); 
+}); 
