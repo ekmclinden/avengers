dbf3558ae0d52fcb6cea8024db1c538219b3ca88
Erin McLinden <ekmclinden@gmail.com>
2020-02-21 00:50:59 -0800

    keep working on js

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,13 @@ $(document).ready(function() {
     var number2 = parseInt($("#add2").val());
     var number3 = parseInt($("#add3").val());
     var result = add(number1, number2, number3);
-    if ((number1 > 0) && (number2 > 0) && (number3 > 0)) {
+    if ((number1 > 0) && ( number2 > 0) && (number3 > 0)) {
       $("#output").text(result); 
+    } else if ((6>= number1) && (6>= number2) && (6>= number3)) {
+      alert ("i'm sad you didn't follow my 1-5 instructions :("); 
+     $("#cheater").show();
+    } else { 
+      alert ("you seriously couldn't fill in 3 boxes?? try again, slacker");
     }
-    else { 
-      alert ("you seriously couldn't fill in 3 boxes?? try again, slacker")
-    }
-  
-   
-
-
-
   }); 
 }); 
