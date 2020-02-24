3165ef15bdce3f6aa3aeb6d467851a9482ce69bf
Erin McLinden <ekmclinden@gmail.com>
2020-02-24 01:23:49 -0800

    edit logic to work better

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,59 @@ $(document).ready(function() {
     var number2 = parseInt($("#add2").val());
     var number3 = parseInt($("#add3").val());
     var result = add(number1, number2, number3);
-    if ((number1 > 0) && ( number2 > 0) && (number3 > 0)) {
-    } else { 
-      alert ("you seriously couldn't fill in 3 boxes?? AHHHHH. Fix it");
-    }
+      if( !$('#add1').val() ) {
+         alert("did you think I'd let you submit this without everything being complete?? THINK AGAIN!!");
+         $("#cap").hide();
+         $("#thor").hide();
+         $("#hulk").hide();
+      }
+      if( !$('#add2').val() ) {
+        alert("did you think I'd let you submit this without everything being complete?? THINK AGAIN!!");
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+     }
+     if( !$('#add3').val() ) {
+      alert("did you think I'd let you submit this without everything being complete?? THINK AGAIN!!"); 
+      $("#cap").hide();
+      $("#thor").hide();
+      $("#hulk").hide();
+   }
     if ( 6>= result) {
       $("#thor").show();
       $("#cap").hide();
       $("#hulk").hide();
+      $(".warning2").hide();
     }
     if (10 > result && result >= 7) {
       $("#cap").show();
       $("#thor").hide();
       $("#hulk").hide();
+      $(".warning2").hide();
     }
     if (11 <= result ) {
       $("#hulk").show();
       $("#thor").hide();
       $("#cap").hide();
+      $(".warning2").hide();
     }
       if (number1 <= 0 || number1 >= 6) {
-        alert ("i'm sorry... is that number 1-5?? follow the RULES");
+        $(".warning2").show();
         $("#cap").hide();
         $("#thor").hide();
         $("#hulk").hide();
     }
+    if (number2 <= 0 || number2 >= 6) {
+      $(".warning2").show();
+      $("#cap").hide();
+      $("#thor").hide();
+      $("#hulk").hide();
+  }
+  if (number3 <= 0 || number3 >= 6) {
+    $(".warning2").show();
+    $("#cap").hide();
+    $("#thor").hide();
+    $("#hulk").hide();
+}
   }); 
 }); 
