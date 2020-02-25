d57db92b42c9c345930f10271989a4e224194f32
Erin McLinden <ekmclinden@gmail.com>
2020-02-24 21:19:54 -0800

    clean up formatting

--- js/scripts.js
+++ js/scripts.js
@@ -13,94 +13,94 @@ $(document).ready(function() {
     var result = add(number1, number2, number3, number4, number5);
       if( !$('#add1').val() ) {
         $(".warning1").show();
-         $("#cap").hide();
-         $("#thor").hide();
-         $("#hulk").hide();
-         $(".warning2").hide();
-   }
-   if( !$('#add2').val() ) {
-    $(".warning1").show();
-    $("#cap").hide();
-    $("#thor").hide();
-    $("#hulk").hide();
-    $(".warning2").hide();
- }
- if( !$('#add3').val() ) {
-  $(".warning1").show();
-  $("#cap").hide();
-  $("#thor").hide();
-  $("#hulk").hide();
-  $(".warning2").hide();
- }
-  if( !$('#add4').val() ) {
-    $(".warning1").show();
-    $("#cap").hide();
-    $("#thor").hide();
-    $("#hulk").hide();
-    $(".warning2").hide();
-  }
-  if( !$('#add5').val() ) {
-    $(".warning1").show();
-    $("#cap").hide();
-    $("#thor").hide();
-    $("#hulk").hide();
-    $(".warning2").hide();
-  }
-    if (10 >= result) {
-      $("#thor").show();
-      $("#cap").hide();
-      $("#hulk").hide();
-      $(".warning2").hide();
-      $(".warning1").hide();
-    }
-    if (17 > result && result >= 11) {
-      $("#cap").show();
-      $("#thor").hide();
-      $("#hulk").hide();
-      $(".warning2").hide();
-      $(".warning1").hide();
-    }
-    if (18 < result) {
-      $("#hulk").show();
-      $("#thor").hide();
-      $("#cap").hide();
-      $(".warning2").hide();
-      $(".warning1").hide();
-    }
-    if (number1 <= 0 || number1 >= 6) {
-      $(".warning2").show();
-      $("#cap").hide();
-      $("#thor").hide();
-      $("#hulk").hide();
-      $(".warning1").hide();
-  }
-  if (number2 <= 0 || number2 >= 6) {
-    $(".warning2").show();
-    $("#cap").hide();
-    $("#thor").hide();
-    $("#hulk").hide();
-    $(".warning1").hide();
-}
-if (number3 <= 0 || number3 >= 6) {
-  $(".warning2").show();
-  $("#cap").hide();
-  $("#thor").hide();
-  $("#hulk").hide();
-  $(".warning1").hide();
-}
-if (number4 <= 0 || number4 >= 6) {
-  $(".warning2").show();
-  $("#cap").hide();
-  $("#thor").hide();
-  $("#hulk").hide();
-  $(".warning1").hide();
-}
-if (number5 <= 0 || number5 >= 6) {
-  $(".warning2").show();
-  $("#cap").hide();
-  $("#thor").hide();
-  $("#hulk").hide();
-  $(".warning1").hide();
-}
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning2").hide();
+       }
+      if( !$('#add2').val() ) {
+        $(".warning1").show();
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning2").hide();
+       }
+      if( !$('#add3').val() ) {
+        $(".warning1").show();
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning2").hide();
+       }
+      if( !$('#add4').val() ) {
+        $(".warning1").show();
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+          $(".warning2").hide();
+        }
+      if( !$('#add5').val() ) {
+        $(".warning1").show();
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning2").hide();
+        }
+      if (10 >= result) {
+        $("#thor").show();
+        $("#cap").hide();
+        $("#hulk").hide();
+        $(".warning2").hide();
+        $(".warning1").hide();
+        }
+      if (17 > result && result >= 11) {
+        $("#cap").show();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning2").hide();
+        $(".warning1").hide();
+       }
+      if (18 < result) {
+        $("#hulk").show();
+        $("#thor").hide();
+        $("#cap").hide();
+        $(".warning2").hide();
+        $(".warning1").hide();
+        }
+      if (number1 <= 0 || number1 >= 6) {
+        $(".warning2").show();
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning1").hide();
+        }
+      if (number2 <= 0 || number2 >= 6) {
+        $(".warning2").show();
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning1").hide();
+        }
+      if (number3 <= 0 || number3 >= 6) {
+        $(".warning2").show();
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning1").hide();
+      }
+      if (number4 <= 0 || number4 >= 6) {
+        $(".warning2").show();
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning1").hide();
+      }
+      if (number5 <= 0 || number5 >= 6) {
+        $(".warning2").show();
+        $("#cap").hide();
+        $("#thor").hide();
+        $("#hulk").hide();
+        $(".warning1").hide();
+      }
   }); 
 }); 
